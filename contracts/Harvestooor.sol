0136c32e36c380fba6ec6b6fb5bfa978ea29bca9
Nate McCord <n8@Nates-MacBook-Pro-2.local>
2021-12-26 12:22:31 -0700

    Added ability to pause selling

--- contracts/Harvestooor.sol
+++ contracts/Harvestooor.sol
@@ -3,13 +3,14 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
 
-contract Harvestooor is Ownable, ERC721Holder, ERC1155Holder {
+contract Harvestooor is Ownable, ERC721Holder, ERC1155Holder, Pausable {
     using SafeERC20 for IERC20;
 
     IERC20 saleToken;
@@ -19,18 +20,18 @@ contract Harvestooor is Ownable, ERC721Holder, ERC1155Holder {
         saleToken = _saleToken;
     }
 
-    function sellERC721(IERC721 _nftContract, uint256 _tokenId) public {
+    function sellERC721(IERC721 _nftContract, uint256 _tokenId) public whenNotPaused {
         _nftContract.safeTransferFrom(msg.sender, address(this), _tokenId);
         saleToken.safeTransfer(msg.sender, ONE_CENT_WEI);
     }
 
-    function sellERC1155(IERC1155 _nftContract, uint256 _tokenId, uint256 _amount) public {
+    function sellERC1155(IERC1155 _nftContract, uint256 _tokenId, uint256 _amount) public whenNotPaused {
         require(_amount <= 25, "Must sell 25 or less NFTs in a single transaction");
         _nftContract.safeTransferFrom(msg.sender, address(this), _tokenId, _amount, "");
         saleToken.safeTransfer(msg.sender, ONE_CENT_WEI * _amount);
     }
 
-    function sellBatchERC1155(IERC1155 _nftContract, uint256[] memory _tokenIds, uint256[] memory _amounts) public {
+    function sellBatchERC1155(IERC1155 _nftContract, uint256[] memory _tokenIds, uint256[] memory _amounts) public whenNotPaused {
         uint256 totalNFTs = 0;
         for (uint256 i = 0; i < _tokenIds.length; i++){
             require(_amounts[i] <= 25, "Must sell 25 or less NFTs of a single ID per transaction");
@@ -54,5 +55,13 @@ contract Harvestooor is Ownable, ERC721Holder, ERC1155Holder {
 
     function emergencyWithdrawERC20(IERC20 _tokenContract, address _receiver, uint256 _value) public onlyOwner {
         _tokenContract.safeTransfer(_receiver, _value);
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner{
+        _unpause();
     }
 }
