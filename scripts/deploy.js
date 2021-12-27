03655a416237e2da8dcb0b6fb3c5f4a2f8ebbc09
Nate McCord <n8@Nates-MacBook-Pro-2.local>
2021-12-27 11:07:22 -0700

    Added support for multiple tokens

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,7 +3,8 @@ const RINKEBY_DAI_ADDRESS = "0x5592EC0cfb4dbc12D3aB100b257153436a1f0FEa"
 
 async function main() {
     const harvestooorFactory = await ethers.getContractFactory("Harvestooor");
-    const harvestooorContract = await harvestooorFactory.deploy(RINKEBY_DAI_ADDRESS, 18);
+    const harvestooorContract = await harvestooorFactory.deploy();
+    await harvestooorContract.supportSaleToken(FRAX_ADDRESS, "10000000000000000");
 
     console.log("Harvestooor deployed to: ", harvestooorContract.address);
 }
--- test/harvestooor.js
+++ test/harvestooor.js
@@ -20,7 +20,8 @@ describe("Harvest contract", function() {
 
     before(async function() {
         const harvestooorFactory = await ethers.getContractFactory("Harvestooor");
-        harvestooorContract = await harvestooorFactory.deploy(FRAX_ADDRESS, 18);
+        harvestooorContract = await harvestooorFactory.deploy();
+        await harvestooorContract.supportSaleToken(FRAX_ADDRESS, "10000000000000000")
 
         await hre.network.provider.request({
             method: "hardhat_impersonateAccount",
@@ -33,11 +34,17 @@ describe("Harvest contract", function() {
     })
 
     it("Sell ERC721 to Harvestooor", async function() {
+        await hre.network.provider.request({
+            method: "hardhat_impersonateAccount",
+            params: ["0xb0151D256ee16d847F080691C3529F316b2D54b3"],
+          });
+        signer = await ethers.getSigner("0xb0151D256ee16d847F080691C3529F316b2D54b3");
+
         const whattyContract = new ethers.Contract(WHATTY_CLUB_ADDRESS, WHATTY_CLUB_ABI, signer);
         await whattyContract.setApprovalForAll(harvestooorContract.address, true);
 
         const userHarvestooor = await harvestooorContract.connect(signer);
-        await userHarvestooor.sellERC721(WHATTY_CLUB_ADDRESS, WHATTY_CLUB_TOKEN_ID);
+        await userHarvestooor.sellERC721(WHATTY_CLUB_ADDRESS, WHATTY_CLUB_TOKEN_ID, FRAX_ADDRESS);
 
         expect(await whattyContract.ownerOf(WHATTY_CLUB_TOKEN_ID)).to.equal(harvestooorContract.address);
     });
@@ -53,10 +60,16 @@ describe("Harvest contract", function() {
         await apeContract.setApprovalForAll(harvestooorContract.address, true);
 
         const userHarvestooor = await harvestooorContract.connect(signer);
-        await truffleAssert.reverts(userHarvestooor.sellERC1155(APEIN_FINANCE_SHOP_ADDRESS, WEAK_HANDS_TOKEN_ID, 30), "Must sell 25 or less NFTs in a single transaction");
+        await truffleAssert.reverts(userHarvestooor.sellERC1155(APEIN_FINANCE_SHOP_ADDRESS, WEAK_HANDS_TOKEN_ID, 30, FRAX_ADDRESS), "Must sell 25 or less NFTs in a single transaction");
     });
 
     it("Sell ERC1155 tokens", async function() {
+        await hre.network.provider.request({
+            method: "hardhat_impersonateAccount",
+            params: ["0x5ebe4c2f14cd027bb2bfce0116cdc4ceb1b68d95"],
+        });
+        signer = await ethers.getSigner("0x5ebe4c2f14cd027bb2bfce0116cdc4ceb1b68d95");
+
         const apeContract = new ethers.Contract(APEIN_FINANCE_SHOP_ADDRESS, APEIN_FINANCE_SHOP_ABI, signer);
         await apeContract.setApprovalForAll(harvestooorContract.address, true);
         const fraxContract = new ethers.Contract(FRAX_ADDRESS, FRAX_ABI, signer);
@@ -64,21 +77,33 @@ describe("Harvest contract", function() {
 
         
         const userHarvestooor = await harvestooorContract.connect(signer);
-        await userHarvestooor.sellERC1155(APEIN_FINANCE_SHOP_ADDRESS, WEAK_HANDS_TOKEN_ID, 3);
+        await userHarvestooor.sellERC1155(APEIN_FINANCE_SHOP_ADDRESS, WEAK_HANDS_TOKEN_ID, 3, FRAX_ADDRESS);
         const afterBal = await fraxContract.balanceOf(signer.address);
 
         expect(afterBal - beforeBal).to.equal(30000000000000000);
     });
 
     it("Batch sell too many ERC1155 tokens", async function() {
+        await hre.network.provider.request({
+            method: "hardhat_impersonateAccount",
+            params: ["0x5ebe4c2f14cd027bb2bfce0116cdc4ceb1b68d95"],
+        });
+        signer = await ethers.getSigner("0x5ebe4c2f14cd027bb2bfce0116cdc4ceb1b68d95");
+
         const apeContract = new ethers.Contract(APEIN_FINANCE_SHOP_ADDRESS, APEIN_FINANCE_SHOP_ABI, signer);
         await apeContract.setApprovalForAll(harvestooorContract.address, true);
 
         const userHarvestooor = await harvestooorContract.connect(signer);
-        await truffleAssert.reverts(userHarvestooor.sellBatchERC1155(APEIN_FINANCE_SHOP_ADDRESS, [4,WEAK_HANDS_TOKEN_ID], [1,30]), "Must sell 25 or less NFTs of a single ID per transaction");
+        await truffleAssert.reverts(userHarvestooor.sellBatchERC1155(APEIN_FINANCE_SHOP_ADDRESS, [4,WEAK_HANDS_TOKEN_ID], [1,30], FRAX_ADDRESS), "Must sell 25 or less NFTs of a single ID per transaction");
     });
 
     it("Batch sell ERC1155 tokens", async function() {
+        await hre.network.provider.request({
+            method: "hardhat_impersonateAccount",
+            params: ["0x5ebe4c2f14cd027bb2bfce0116cdc4ceb1b68d95"],
+        });
+        signer = await ethers.getSigner("0x5ebe4c2f14cd027bb2bfce0116cdc4ceb1b68d95");
+
         const apeContract = new ethers.Contract(APEIN_FINANCE_SHOP_ADDRESS, APEIN_FINANCE_SHOP_ABI, signer);
         await apeContract.setApprovalForAll(harvestooorContract.address, true);
         const fraxContract = new ethers.Contract(FRAX_ADDRESS, FRAX_ABI, signer);
@@ -86,13 +111,19 @@ describe("Harvest contract", function() {
 
         
         const userHarvestooor = await harvestooorContract.connect(signer);
-        await userHarvestooor.sellBatchERC1155(APEIN_FINANCE_SHOP_ADDRESS, [4,WEAK_HANDS_TOKEN_ID], [1,20])
+        await userHarvestooor.sellBatchERC1155(APEIN_FINANCE_SHOP_ADDRESS, [4,WEAK_HANDS_TOKEN_ID], [1,20], FRAX_ADDRESS)
         const afterBal = await fraxContract.balanceOf(signer.address);
 
         expect(afterBal - beforeBal).to.equal(210000000000000000);
     });
 
     it("Non-owner attempted Emergency Withdraw ERC721", async function() {
+        await hre.network.provider.request({
+            method: "hardhat_impersonateAccount",
+            params: ["0x5ebe4c2f14cd027bb2bfce0116cdc4ceb1b68d95"],
+        });
+        signer = await ethers.getSigner("0x5ebe4c2f14cd027bb2bfce0116cdc4ceb1b68d95");
+
         const userHarvestooor = await harvestooorContract.connect(signer);
         await truffleAssert.reverts(userHarvestooor.emergencyWithdrawERC721(WHATTY_CLUB_ADDRESS, WHATTY_CLUB_TOKEN_ID, "0x5ebe4c2f14cd027bb2bfce0116cdc4ceb1b68d95"), "Ownable: caller is not the owner");
     });
@@ -106,5 +137,13 @@ describe("Harvest contract", function() {
         await whattyContract.setApprovalForAll(harvestooorContract.address, true);
 
         expect(await whattyContract.ownerOf(WHATTY_CLUB_TOKEN_ID)).to.equal("0x5EbE4C2F14cd027bB2BfcE0116cDC4cEB1b68d95");
+    })
+
+    it("Remove token support", async function() {
+        signer = await ethers.getSigner(0);
+
+        expect(parseInt(await harvestooorContract.getCurrentOneCentWeiValue(FRAX_ADDRESS))).to.be.greaterThan(0);
+        await harvestooorContract.removeSaleToken(FRAX_ADDRESS);
+        expect(parseInt(await harvestooorContract.getCurrentOneCentWeiValue(FRAX_ADDRESS))).to.equal(0);
     })
 });
